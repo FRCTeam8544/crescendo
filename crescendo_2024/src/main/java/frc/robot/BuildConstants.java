b45d43b3146cc9a21095d64c2d2d804eb02badd6
Bobble Hat <lu.dru1459@gmail.com>
2024-02-10 10:46:34 -0500

    added ModuleIO for spark

--- crescendo_2024/src/main/java/frc/robot/BuildConstants.java
+++ crescendo_2024/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "crescendo_2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 45;
-  public static final String GIT_SHA = "7c0596adbd29fa62b2d1824f670b8497df2450af";
-  public static final String GIT_DATE = "2024-02-05 18:44:32 EST";
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "341b952df59a7cf2806c2b1b030a1fa64cb04746";
+  public static final String GIT_DATE = "2024-02-10 10:22:52 EST";
   public static final String GIT_BRANCH = "feature_AdvantageKit";
-  public static final String BUILD_DATE = "2024-02-09 20:29:51 EST";
-  public static final long BUILD_UNIX_TIME = 1707528591514L;
+  public static final String BUILD_DATE = "2024-02-10 10:39:01 EST";
+  public static final long BUILD_UNIX_TIME = 1707579541598L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- crescendo_2024/src/main/java/frc/robot/io_bs/IO_Sim.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.io_bs;
-
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-
-public class IO_Sim {
-  private DCMotorSim m_driveSim = new DCMotorSim(DCMotor.getNEO(1), 6.75, 0.025);
-  private DCMotorSim m_turnSim = new DCMotorSim(DCMotor.getNEO(1), 150.0 / 7.0, 0.004);
-}
